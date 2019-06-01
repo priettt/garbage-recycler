788b6644dd26927feadd3e098c2bd1fbd1c652b3
Francisco Prieto <francisco.prieto@globant.com>
2019-06-01 16:33:22 -0300

    Finished UI

--- app/src/main/java/com/unicen/garbage/ui/main/HistoryFragment.java
+++ app/src/main/java/com/unicen/garbage/ui/main/HistoryFragment.java
@@ -9,9 +9,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.unicen.garbage.R;
 import com.unicen.garbage.domain.RecyclingRepository;
+import com.unicen.garbage.domain.entities.Recycling;
+
+import java.util.ArrayList;
 
 public class HistoryFragment extends Fragment {
 
@@ -27,9 +31,26 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history, container, false);
         Context context = view.getContext();
+        final RecyclingAdapter adapter = new RecyclingAdapter(RecyclingRepository.getRecyclingHistory());
         RecyclerView recyclerView = view.findViewById(R.id.history_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
-        recyclerView.setAdapter(new RecyclingAdapter(RecyclingRepository.getRecyclingHistory()));
+        recyclerView.setAdapter(adapter);
+        Button refreshButton = view.findViewById(R.id.history_refresh_button);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                ArrayList<Recycling> aux = new ArrayList<>();
+                aux.add(new Recycling("hola", "hola", "hola", "hola", "hola", "hola"));
+                aux.add(new Recycling("hola", "hola", "hola", "hola", "hola", "hola"));
+                aux.add(new Recycling("hola", "hola", "hola", "hola", "hola", "hola"));
+                aux.add(new Recycling("hola", "hola", "hola", "hola", "hola", "hola"));
+                aux.add(new Recycling("hola", "hola", "hola", "hola", "hola", "hola"));
+                aux.add(new Recycling("hola", "hola", "hola", "hola", "hola", "hola"));
+                aux.add(new Recycling("hola", "hola", "hola", "hola", "hola", "hola"));
+                aux.add(new Recycling("hola", "hola", "hola", "hola", "hola", "hola"));
+
+                adapter.updateItems(aux);
+            }
+        });
         return view;
     }
 }
